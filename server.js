1f003b15b5a1d824e437da0e978ecac9b6b7c381
leulkashay <leulkashay2012@gmail.com>
2023-09-05 23:39:23 -0700

    new task

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import startServer from './libs/boot';
-import injectRoutes from './route';
+import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
 
 const server = express();
